82049adcc3915bb1be935e9b407526352f8d050c
Africa Void <72352225+africavoid@users.noreply.github.com>
2023-07-30 11:08:40 +0100

    Add files via upload

--- FreeBSD.sh
+++ FreeBSD.sh
@@ -1,7 +1,8 @@
 # Install script for packages on FreeBSD
-pkg install git cgdb gdb vim vifm mixer mixertui xorg tmux qutebrowser terminus-ttf terminus-font mupdf gcc tcc doas lsblk w3m tudu ncdu gnustep nasm tty-clock mpv
+pkg install git cgdb gdb vim vifm mixer mixertui xorg tmux qutebrowser terminus-ttf terminus-font mupdf gcc tcc doas lsblk w3m tudu ncdu gnustep nasm tty-clock mpv groff bvi
 
 # Uncomment if you didnt install the ports tree at first install
 # git clone https://git.FreeBSD.org/ports.git /usr/ports
 
 # Remember to install xsel (x11/xsel-conrad) through ports
+# Remember to install wpa_supplicant_gui (net/wpa_supplicant/gui) through ports 
