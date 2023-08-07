0b7cb6baa0f024cc6e483529a331df050b33a981
Africa Void <72352225+africavoid@users.noreply.github.com>
2023-08-07 20:16:25 +0100

    Add files via upload

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,17 @@
+-- Function for setting keybinds
+local map = vim.keymap.set
 
+-- Function for startup options
+local opt = vim.cmd
+
+-- Some nice keybinds 
+map("n", "<C-s>", "<Cmd>split | terminal<CR>")
+map("n", "<C-p>", "<Cmd>Lexplore<CR>")
+
+-- Set the line number color
+vim.api.nvim_set_hl(0, 'LineNr', {fg = white} )
+
+-- Setup options
+opt("syntax off")
+opt("set laststatus=3")
+opt("set number")
